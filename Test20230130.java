072cd7ba8bb5855ce3d1b18775fa7867ff41ff8f
minah-moon <118171802+minah-moon@users.noreply.github.com>
2023-02-13 13:49:35 +0900

    20230213

--- Test20230130.java
+++ Test20230130.java
@@ -5,21 +5,26 @@ import java.util.*;
 public class Test20230130 {
 	public static void main(String[] args) {
 		Solution solution = new Solution();
-		int[] numbers = {2,1,3,4,1};
-		System.out.println(solution.solution(numbers));
+		int[] n = {-10, 30, -20, 10, 40, -50, 20, 70};
+		System.out.println(solution.solution(n));
 		
 	}
 }
 
+
 class Solution {
-    public int[] solution(int[] numbers) {
-        int[] answer = {};
-        for (int i = 0; i < numbers.length-1; i++) {
-			for (int j = i+1; j < numbers.length; j++) {
-				int a = numbers[i] + numbers[j];
-				System.out.print(a);
+    public int solution(int[] number) {
+        int answer = 0;
+       
+        for (int i = 0; i < number.length-2; i++) {
+			for (int j = i+1; j < number.length-1; j++) {
+				for (int k = j+1; k < number.length; k++) {
+					if(number[i]+number[j]+number[k] == 0) answer++; 
+					
+				}
+				
 			}
 		}
-       return answer;
+        return answer;
     }
 }
